e057e0637c7e344c4d39aea29a5c5dfacdc831fc
YoungPhilGim <youngphil.gim@gmail.com>
2023-02-03 16:45:16 +0900

    Update 03.js

--- 03.js
+++ 03.js
@@ -19,4 +19,4 @@ const numbers = [
 const zero_bin = [0b01110, 0b10001, 0b10011, 0b10101, 0b11001, 0b10001, 0b01110];
 const zero_oct = [   0o16,    0o21,    0o23,    0o25,    0o31,    0o21,    0o16];
 const zero_dec = [     14,      17,      19,      21,      25,      17,      14];
-const zero_hex = [   0x0E,    0x11,    0x13,    0x15,    0x1F,    0x11,    0x0E];
+const zero_hex = [   0x0E,    0x11,    0x13,    0x15,    0x19,    0x11,    0x0E];
